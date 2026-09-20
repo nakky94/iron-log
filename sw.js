0d51b6710973475f756ae20ed5b8289d8bd6c729
nakky94 <jamakro@gmail.com>
2026-09-21 07:46:13 +1000

    Bust cache for new yellow icon

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = "gym-log-v1";
+const CACHE = "gym-log-v2";
 const ASSETS = ["/", "/index.html", "/app.js", "/manifest.webmanifest", "/icon.svg"];
 self.addEventListener("install", (event) => {
   event.waitUntil(caches.open(CACHE).then((c) => c.addAll(ASSETS)).then(() => self.skipWaiting()));
