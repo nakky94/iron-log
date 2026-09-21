d4ffb55fd5fcbd943d5e8366d4886bbad6b8d44d
nakky94 <jamakro@gmail.com>
2026-09-21 20:08:00 +1000

    Cache bust v36 Train gym UI

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = "gym-log-v35";
+const CACHE = "gym-log-v36";
 const ASSETS = ["/", "/index.html", "/app.js", "/suggest.js", "/extra.js", "/more.js", "/cues.js", "/backup.js", "/homeui.js", "/trainsets.js", "/parse.worker.js", "/manifest.webmanifest", "/icon.svg"];
 self.addEventListener("install", (event) => {
   event.waitUntil(caches.open(CACHE).then((c) => c.addAll(ASSETS)).then(() => self.skipWaiting()));
