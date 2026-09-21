a59ae53d6a90b65adccb12df30d4741491293262
nakky94 <jamakro@gmail.com>
2026-09-21 17:50:10 +1000

    Cache bust v12 after design pass

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = "gym-log-v11";
+const CACHE = "gym-log-v12";
 const ASSETS = ["/", "/index.html", "/app.js", "/suggest.js", "/extra.js", "/more.js", "/cues.js", "/manifest.webmanifest", "/icon.svg", "/fitnotes.csv"];
 self.addEventListener("install", (event) => {
   event.waitUntil(caches.open(CACHE).then((c) => c.addAll(ASSETS)).then(() => self.skipWaiting()));
