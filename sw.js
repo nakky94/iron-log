d778f96bfc7893979e05208c9e351ab644abebb2
nakky94 <jamakro@gmail.com>
2026-09-21 18:12:37 +1000

    Cache-first paint: active tab only, capped log, v17

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = "gym-log-v16";
+const CACHE = "gym-log-v17";
 const ASSETS = ["/", "/index.html", "/app.js", "/suggest.js", "/extra.js", "/more.js", "/cues.js", "/backup.js", "/homeui.js", "/manifest.webmanifest", "/icon.svg"];
 self.addEventListener("install", (event) => {
   event.waitUntil(caches.open(CACHE).then((c) => c.addAll(ASSETS)).then(() => self.skipWaiting()));
