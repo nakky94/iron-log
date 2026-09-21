b26bc047234ef0c513a701401c7cc495f3007912
nakky94 <jamakro@gmail.com>
2026-09-21 19:15:48 +1000

    Cache bust v31 no-flash Home

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = "gym-log-v30";
+const CACHE = "gym-log-v31";
 const ASSETS = ["/", "/index.html", "/app.js", "/suggest.js", "/extra.js", "/more.js", "/cues.js", "/backup.js", "/homeui.js", "/parse.worker.js", "/manifest.webmanifest", "/icon.svg"];
 self.addEventListener("install", (event) => {
   event.waitUntil(caches.open(CACHE).then((c) => c.addAll(ASSETS)).then(() => self.skipWaiting()));
