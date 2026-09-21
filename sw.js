19cef85aeb0b353d296c5d3ffe99fbf0dc310f64
nakky94 <jamakro@gmail.com>
2026-09-21 20:18:44 +1000

    Cache bust v38 perf memo

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE = "gym-log-v37";
-const ASSETS = ["/", "/index.html", "/app.js", "/suggest.js", "/extra.js", "/more.js", "/cues.js", "/backup.js", "/homeui.js", "/trainsets.js", "/parse.worker.js", "/manifest.webmanifest", "/icon.svg"];
+const CACHE = "gym-log-v38";
+const ASSETS = ["/", "/index.html", "/perf.js", "/app.js", "/suggest.js", "/extra.js", "/more.js", "/cues.js", "/backup.js", "/homeui.js", "/trainsets.js", "/parse.worker.js", "/manifest.webmanifest", "/icon.svg"];
 self.addEventListener("install", (event) => {
   event.waitUntil(caches.open(CACHE).then((c) => c.addAll(ASSETS)).then(() => self.skipWaiting()));
 });
