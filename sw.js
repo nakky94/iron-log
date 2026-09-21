7f07692e330f1e629c7818859ecb464a913e75ea
nakky94 <jamakro@gmail.com>
2026-09-21 15:11:37 +1000

    Cache bust v9 after cue/backup change

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = "gym-log-v8";
+const CACHE = "gym-log-v9";
 const ASSETS = ["/", "/index.html", "/app.js", "/suggest.js", "/extra.js", "/more.js", "/cues.js", "/manifest.webmanifest", "/icon.svg", "/fitnotes.csv"];
 self.addEventListener("install", (event) => {
   event.waitUntil(caches.open(CACHE).then((c) => c.addAll(ASSETS)).then(() => self.skipWaiting()));
