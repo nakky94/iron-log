114705ee987cc319870d6a96a57cc358790f4f2d
nakky94 <jamakro@gmail.com>
2026-09-22 13:25:33 +1000

    Cache bust v45 typefix

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE = "gym-log-v44";
-const ASSETS = ["/", "/index.html", "/perf.js", "/app.js", "/suggest.js", "/extra.js", "/more.js", "/cues.js", "/backup.js", "/homeui.js", "/trainsets.js", "/restfix.js", "/volume.js", "/plus.js", "/parse.worker.js", "/manifest.webmanifest", "/icon.svg"];
+const CACHE = "gym-log-v45";
+const ASSETS = ["/", "/index.html", "/perf.js", "/app.js", "/suggest.js", "/extra.js", "/more.js", "/cues.js", "/backup.js", "/homeui.js", "/trainsets.js", "/restfix.js", "/volume.js", "/plus.js", "/typefix.js", "/parse.worker.js", "/manifest.webmanifest", "/icon.svg"];
 self.addEventListener("install", (event) => {
   event.waitUntil(caches.open(CACHE).then((c) => c.addAll(ASSETS)).then(() => self.skipWaiting()));
 });
